e2f91acb8d8cc680e2dd87f5fe5b2242ffbe557a
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-04-24 00:16:05 +0800

    #36 新增修改電話頁面UI

---
+++ App/app/src/main/java/fcu/app/kebukepsyduck/RevisePhoneNumberActivity.java
@@ -0,0 +1,14 @@
+package fcu.app.kebukepsyduck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RevisePhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revise_phone_number);
+    }
+}
