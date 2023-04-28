a02b62675a5b797a52c33958ef86fc258be012cc
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-04-29 00:12:53 +0800

    #47 新增訂單管理的訂單明細頁面 (#49)
    * feat: add order details page
    
    * feat: test intent link to order detail page
    
    * update order manage id and component
    
    * update order details page component layout

---
+++ App/app/src/main/java/fcu/app/kebukepsyduck/OrderDetailsActivity.java
@@ -0,0 +1,16 @@
+package fcu.app.kebukepsyduck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+
+}
--- App/app/src/main/java/fcu/app/kebukepsyduck/OrderManageActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/OrderManageActivity.java
@@ -2,13 +2,27 @@ package fcu.app.kebukepsyduck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OrderManageActivity extends AppCompatActivity {
+
+    Button order1Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_manage);
+
+        order1Btn = findViewById(R.id.btn_orderManage_order1);
+        order1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderManageActivity.this, OrderDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
