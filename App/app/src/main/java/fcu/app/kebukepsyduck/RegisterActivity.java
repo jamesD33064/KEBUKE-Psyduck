615560de3b54ee60c6fb3a30688caa8f063310a2
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-05-13 11:01:25 +0800

    feat: add register function

--- App/app/src/main/java/fcu/app/kebukepsyduck/RegisterActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/RegisterActivity.java
@@ -2,13 +2,44 @@ package fcu.app.kebukepsyduck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText registerPhoneNumber;
+    EditText registerPassword;
+    Button registerBtn;
+    DatabaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        registerPhoneNumber = findViewById(R.id.et_register_phoneNumber);
+        registerPassword = findViewById(R.id.et_register_password);
+        registerBtn = findViewById(R.id.btn_register);
+        db = new DatabaseHandler(this);
+        db.open();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = registerPhoneNumber.getText().toString();
+                String password = registerPassword.getText().toString();
+                if (phoneNumber.equals("") || password.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "請輸入電話號碼和密碼", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                db.addMember(phoneNumber, password);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
