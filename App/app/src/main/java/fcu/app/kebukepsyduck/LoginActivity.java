961bd05494d0dcf505e3cdf8996e2324a44767bf
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-05-13 11:00:26 +0800

    feat: add intent jump to register page

--- App/app/src/main/java/fcu/app/kebukepsyduck/LoginActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/LoginActivity.java
@@ -2,14 +2,28 @@ package fcu.app.kebukepsyduck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView loginRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginRegister = findViewById(R.id.tv_login_register);
+        loginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
