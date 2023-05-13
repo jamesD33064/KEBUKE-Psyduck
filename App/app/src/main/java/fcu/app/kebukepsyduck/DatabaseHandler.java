14711cb0e9606edf626da3b690fc08e1d40d8b54
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-05-13 11:06:15 +0800

    #53 新增用戶註冊功能
    * feat: create DatabaseHandler
    
    * feat: add intent jump to register page
    
    * update: change main page to login page
    
    * feat: add register function
    
    * update: change main page to MainActivity

---
+++ App/app/src/main/java/fcu/app/kebukepsyduck/DatabaseHandler.java
@@ -0,0 +1,66 @@
+package fcu.app.kebukepsyduck;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DatabaseHandler {
+    private AppCompatActivity activity;
+
+    private SQLiteDatabase database;
+
+    private static final String DATABASE_NAME = "kebukePsyduck.db";
+
+    private static final String CREATE_MEMBER_TABLE = "CREATE TABLE IF NOT EXISTS Members (" +
+            " _id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            " phone TEXT NOT NULL UNIQUE, " +
+            " password TEXT NOT NULL)";
+
+    public DatabaseHandler(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    public void open() {
+        database = activity.openOrCreateDatabase(DATABASE_NAME, Context.MODE_PRIVATE, null);
+        database.execSQL(CREATE_MEMBER_TABLE);
+    }
+
+    public void addMember(String phone, String password) {
+        Cursor cursor = database.rawQuery("SELECT * FROM Members WHERE phone=?", new String[]{phone});
+        if (cursor.moveToFirst()) {
+            // A record with the same phone number already exists
+            Toast.makeText(activity, "此手機號碼已被註冊", Toast.LENGTH_SHORT).show();
+        } else {
+            // No record with the same phone number exists, insert the new member
+            ContentValues values = new ContentValues();
+            values.put("phone", phone);
+            values.put("password", password);
+            database.insert("Members", null, values);
+            Toast.makeText(activity, "註冊成功", Toast.LENGTH_SHORT).show();
+        }
+        cursor.close();
+    }
+
+    public Cursor getAllMember() {
+        Cursor cursor = database.rawQuery("SELECT * FROM Members", null);
+        return cursor;
+    }
+
+    public String getMemberPassword(String phone) {
+        Cursor cursor = database.query("Members", new String[] {"password"}, "phone=?", new String[] {phone}, null, null, null);
+        if (cursor.moveToFirst()) {
+            String password = cursor.getString(0);
+            cursor.close();
+            return password;
+        } else {
+            cursor.close();
+            return null;
+        }
+    }
+}
