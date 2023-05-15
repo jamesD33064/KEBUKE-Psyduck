0f5ea0cfb9a71f961d08891b20ded590644961e6
D33064 <james0120160379@gmail.com>
2023-05-16 01:04:44 +0800

    #64 新增由品項列表跳轉至品項資訊

--- App/app/src/main/java/fcu/app/kebukepsyduck/FoodListActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/FoodListActivity.java
@@ -22,6 +22,12 @@ public class FoodListActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityFoodListBinding binding;
 
+    private component_FoodItem item1;
+    private component_FoodItem item2;
+    private component_FoodItem item3;
+    private component_FoodItem item4;
+//    private component_FoodItem[] foodItem_list = {item1,item2,item3,item4};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +43,47 @@ public class FoodListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+        item1 = findViewById(R.id.FoodItem_1);
+        item2 = findViewById(R.id.FoodItem_2);
+        item3 = findViewById(R.id.FoodItem_3);
+        item4 = findViewById(R.id.FoodItem_4);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent();
+                intent.setClass(FoodListActivity.this,ProductActivity.class);
+                switch (v.getId()){
+                    case R.id.FoodItem_1:
+                        bundle.putString("FoodName","高級蛋餅");
+                        bundle.putString("FoodPrice","120$");
+                        break;
+                    case R.id.FoodItem_2:
+                        bundle.putString("FoodName","漢堡");
+                        bundle.putString("FoodPrice","130$");
+                        break;
+                    case R.id.FoodItem_3:
+                        bundle.putString("FoodName","吐司");
+                        bundle.putString("FoodPrice","140$");
+                        break;
+                    case R.id.FoodItem_4:
+                        bundle.putString("FoodName","奶茶");
+                        bundle.putString("FoodPrice","150$");
+                        break;
+                }
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        };
+        item1.setOnClickListener(listener);
+        item2.setOnClickListener(listener);
+        item3.setOnClickListener(listener);
+        item4.setOnClickListener(listener);
+
+
     }
 
 }
--- App/app/src/main/java/fcu/app/kebukepsyduck/ProductActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/ProductActivity.java
@@ -3,12 +3,23 @@ package fcu.app.kebukepsyduck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProductActivity extends AppCompatActivity {
 
+    private TextView FoodName;
+    private TextView FoodPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        FoodName = findViewById(R.id.product_detail_name);
+        FoodPrice = findViewById(R.id.product_detail_price);
+
+        FoodName.setText(getIntent().getExtras().getString("FoodName"));
+        FoodPrice.setText(getIntent().getExtras().getString("FoodPrice"));
+
+
     }
 }
