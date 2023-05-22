864c6d52a6c4d621e3414ff156c4c48575670c71
jamesD33064 <james0120160379@gmail.com>
2023-05-22 14:03:58 +0800

    #78 購物車頁面僅顯示同品項加總結果

--- App/app/src/main/java/fcu/app/kebukepsyduck/CartActivity.java
+++ App/app/src/main/java/fcu/app/kebukepsyduck/CartActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -55,17 +59,39 @@ public class CartActivity extends AppCompatActivity {
 
     private void showAllCartItem() {
         SharedPreferences sharedPref = getSharedPreferences("loginPref", MODE_PRIVATE);
-        String PhoneNumber = sharedPref.getString("phoneNumber", "");
-        Cursor cursor = db.getAllCartItem(PhoneNumber);
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-                CartActivity.this,
-                android.R.layout.simple_list_item_2,
-                cursor,
-                new String[] { "item_name", "quantity" },
-//      new String[] { "meal_name", "img_name" },
-                new int[] { android.R.id.text1, android.R.id.text2},
-                0
+        String phoneNumber = sharedPref.getString("phoneNumber", "");
+        Cursor cursor = db.getAllCartItem(phoneNumber);
+
+        Map<String, Integer> quantityMap = new HashMap<>();
+
+        while (cursor.moveToNext()) {
+            String itemName = cursor.getString(cursor.getColumnIndex("item_name"));
+            int quantity = cursor.getInt(cursor.getColumnIndex("quantity"));
+
+            if (quantityMap.containsKey(itemName)) {
+                quantity += quantityMap.get(itemName);
+            }
+            quantityMap.put(itemName, quantity);
+        }
+
+        List<Map<String, String>> data = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : quantityMap.entrySet()) {
+            String itemName = entry.getKey();
+            String quantity = String.valueOf(entry.getValue());
+            Map<String, String> itemData = new HashMap<>();
+            itemData.put("item_name", itemName);
+            itemData.put("quantity", quantity);
+            data.add(itemData);
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(
+          CartActivity.this,
+          data,
+          android.R.layout.simple_list_item_2,
+          new String[]{"item_name", "quantity"},
+          new int[]{android.R.id.text1, android.R.id.text2}
         );
         lv_cart_OrderFoods.setAdapter(adapter);
     }
+
 }
